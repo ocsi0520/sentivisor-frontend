a11cb888ac1d2bbd949b5e08c51467314ca3de46
Faragó Tamás <ocsi0520@gmail.com>
2025-10-16 11:52:27 +0200

    fix injecting scripts into already opened pages (by reloading)

--- src/worker/inject-scripts-into-tabs.ts
+++ src/worker/inject-scripts-into-tabs.ts
@@ -1,41 +1,15 @@
-type ContentScript = NonNullable<
-  chrome.runtime.Manifest["content_scripts"]
->[number];
-
-const injectScriptsIntoTab = async (
-  tab: chrome.tabs.Tab,
-  files: string[] | undefined
-): Promise<void> => {
-  if (!tab.id || !files) return;
-
-  try {
-    await chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files,
-    });
-    console.log("Injected into:", tab.url);
-  } catch (err) {
-    console.warn("Could not inject into", tab.url, err);
-  }
-};
-
-const handleContentScript = async (
-  aContentScript: ContentScript
-): Promise<void> => {
-  const matches = aContentScript.matches || [];
-  const tabs = await chrome.tabs.query({ url: matches });
-
-  for (const tab of tabs) {
-    await injectScriptsIntoTab(tab, aContentScript.js);
-  }
+const getAllMatchers = (): Array<string> => {
+  const manifest = chrome.runtime.getManifest();
+  const allJoinedMatchers = (manifest.content_scripts || [])
+    .map((aContentScript) => aContentScript.matches || [])
+    .reduce((sum, matchesArray) => [...sum, ...matchesArray], []);
+  return Array.from(new Set(allJoinedMatchers));
 };
 
 export const injectContentScriptIntoAlreadyOpenedPages =
   async (): Promise<void> => {
-    const manifest = chrome.runtime.getManifest();
-    const contentScripts = manifest.content_scripts || [];
-
-    for (const contentScript of contentScripts) {
-      handleContentScript(contentScript);
-    }
+    const tabs = await chrome.tabs.query({ url: getAllMatchers() });
+    return Promise.all(tabs.map((tab) => chrome.tabs.reload(tab.id!))).then(
+      () => {}
+    );
   };
